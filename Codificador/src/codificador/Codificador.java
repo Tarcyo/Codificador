57a5d05a590d91bc18eb10178085b04ec1043fbd
Tarcyo <55460663+Tarcyo@users.noreply.github.com>
2022-08-15 14:38:07 -0300

    Add files via upload

---
+++ Codificador/src/codificador/Codificador.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codificador;
+
+/**
+ *
+ * @author Tarcyo
+ */
+public class Codificador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        TelaDoCodificador t= new TelaDoCodificador();
+        t.setVisible(true);
+    }
+    
+}
